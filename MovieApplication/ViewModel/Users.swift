b0cca585aca53b2b5f5aa5a0ae072e1acf1c3ec0
Dilara Karadaş <102139155+dlrkaradas@users.noreply.github.com>
2024-08-27 14:53:35 +0300

    .

---
+++ MovieApplication/ViewModel/Users.swift
@@ -0,0 +1,19 @@
+//
+//  Users.swift
+//  MovieApplication
+//
+//  Created by Hilal Karadas on 13.08.2023.
+//
+
+import Foundation
+struct Users{
+    var isim : String
+    var soyisim : String
+    var password : Int
+    var email : String
+    var id : Int
+ 
+
+        
+    }
+
